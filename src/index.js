53d55e22dd0e601d6eafc07f14e05574f2a2a20f
Dean Netherton <dean.netherton@gmail.com>
2017-05-02 14:40:25 +1000

    Added operator replayLastValue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+import {Observable} from 'rxjs/Observable'
+import {Subject} from 'rxjs/Subject'
 
-export function index() {
+function nullFunction() {
 }
+
+export function replayLastValue() {
+  let captured = undefined
+
+  const observable = Observable.create(observer => {
+    if(captured !== undefined)
+      observer.next(captured)
+
+    return this.subscribe({
+      next: v => {
+        captured = v
+        observer.next(v)
+      },
+      error: err => observer.error(err),
+      complete: () => observer.complete()
+    })
+  })
+
+  observable.subscribe({next: nullFunction})
+
+  return observable
+}
+
--- test/index_spec.js
+++ test/index_spec.js
@@ -1,3 +1,28 @@
-describe('A Test', () => {
-  it('verify something', () => expect(1).to.equal(1))
+import { replayLastValue } from './../lib/index'
+import { Subject } from 'rxjs/Subject'
+
+function collect(observer, store) {
+  store.length = 0
+  const unsub = observer.subscribe(x => store.push(x))
+  return () => unsub.unsubscribe()
+}
+
+describe('rx_operators', () => {
+
+  it('replayLastValue', () => {
+    const sub = new Subject()
+    const souceReplayed = sub::replayLastValue()
+
+    const result = []
+    collect(souceReplayed, result)()
+    expect(result).to.deep.equal([])
+
+    sub.next(2)
+    sub.next(3)
+
+    collect(souceReplayed, result)()
+
+    expect(result).to.deep.equal([3])
+  })
+
 })
